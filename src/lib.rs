845253f51aec7794ea88b9bffdc8255499011fd1
amogh yermalkar <amogh.yermalkar@sugarboxnetworks.com>
2023-02-17 22:03:07 +0530

    Add: examples for docs

--- src/lib.rs
+++ src/lib.rs
@@ -14,22 +14,53 @@ pub struct KvStore {
 /// KvStore methods
 impl KvStore {
     /// creates a new instance of the memory store
+    /// 
+    /// # Example
+    /// 
+    /// ```rust
+    ///  let mut kvs = kvs::KvStore::new();
+    /// // your logic here
+    /// ```
     pub fn new() -> Self {
         KvStore { map: HashMap::new() }
     }
 
     /// insert the key value pair in memory
+    /// 
+    /// # Example
+    /// 
+    /// ```rust
+    /// let mut kvs = kvs::KvStore::new();
+    /// let key = "key".to_owned();
+    /// let val = "val".to_owned();
+    /// kvs.set(key.to_owned(), val.to_owned())
+    /// ```
     pub fn set(&mut self, key: String, val: String) {
         self.map.insert(key, val);
     }
 
     /// get the value by key
+    /// 
+    /// # Example
+    /// 
+    /// ```rust
+    /// let mut kvs = kvs::KvStore::new();
+    /// let val = "val".to_owned();
+    /// kvs.get(val.to_owned());
+    /// ```
     pub fn get(&self, key: String) -> Option<String> {
        let ans = self.map.get(&key);
        ans.to_owned().cloned()
     }
 
     /// remove a key value pair, accepts key
+    /// 
+    /// # Example
+    /// ```rust
+    /// let mut kvs = kvs::KvStore::new();
+    /// let key = "key".to_owned();
+    /// kvs.remove(key.to_owned());
+    /// ```
     pub fn remove(&mut self, key: String) {
         self.map.remove(&key);
     }
