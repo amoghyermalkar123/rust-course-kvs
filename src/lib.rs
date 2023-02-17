f59e7a5715d10e797daf90b14d6fa2c3a3656b85
amogh yermalkar <amogh.yermalkar@sugarboxnetworks.com>
2023-02-17 21:57:07 +0530

    Add: Docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,35 @@
+#![deny(missing_docs)]
 #![allow(dead_code)]
 
-use std::{collections::HashMap};
+//! this is the kv store implementation
+//! stores data in memory
+
+use std::collections::HashMap;
+
+/// KvStore is the memory store
 pub struct KvStore {
     map: HashMap<String, String>
 }
 
+/// KvStore methods
 impl KvStore {
+    /// creates a new instance of the memory store
     pub fn new() -> Self {
         KvStore { map: HashMap::new() }
     }
 
+    /// insert the key value pair in memory
     pub fn set(&mut self, key: String, val: String) {
         self.map.insert(key, val);
     }
 
+    /// get the value by key
     pub fn get(&self, key: String) -> Option<String> {
        let ans = self.map.get(&key);
        ans.to_owned().cloned()
     }
 
+    /// remove a key value pair, accepts key
     pub fn remove(&mut self, key: String) {
         self.map.remove(&key);
     }
